60bd1311a172aef991e214dceb65877716770807
liulixiang1988 <liulixiang1988@gmail.com>
2013-11-27 09:53:03 +0800

    设置NSURLCache和AFNetworkActivityIndicatorManager

--- AFNetworkingExapmple/AFNetworkingExapmple/LLXAppDelegate.m
+++ AFNetworkingExapmple/AFNetworkingExapmple/LLXAppDelegate.m
@@ -7,12 +7,19 @@
 //
 
 #import "LLXAppDelegate.h"
+#import "AFNetworkActivityIndicatorManager.h"
 
 @implementation LLXAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    NSURLCache *urlCache = [[NSURLCache alloc] initWithMemoryCapacity: 4*1024*1024
+                                                         diskCapacity: 20*1024*1024
+                                                             diskPath:nil];
+    [NSURLCache setSharedURLCache:urlCache];
+    [[AFNetworkActivityIndicatorManager sharedManager] setEnabled:YES];
+    
     return YES;
 }
 							
