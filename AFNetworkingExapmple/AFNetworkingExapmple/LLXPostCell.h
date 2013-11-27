8c28d1592a38f4d9e4886c8f9a176728c429c18b
liulixiang1988 <liulixiang1988@gmail.com>
2013-11-27 10:01:12 +0800

    添加了PostCell

---
+++ AFNetworkingExapmple/AFNetworkingExapmple/LLXPostCell.h
@@ -0,0 +1,13 @@
+//
+//  LLXPostCell.h
+//  AFNetworkingExapmple
+//
+//  Created by 刘理想 on 13-11-27.
+//  Copyright (c) 2013年 刘理想. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LLXPostCell : UITableViewCell
+
+@end
---
+++ AFNetworkingExapmple/AFNetworkingExapmple/LLXPostCell.m
@@ -0,0 +1,29 @@
+//
+//  LLXPostCell.m
+//  AFNetworkingExapmple
+//
+//  Created by 刘理想 on 13-11-27.
+//  Copyright (c) 2013年 刘理想. All rights reserved.
+//
+
+#import "LLXPostCell.h"
+
+@implementation LLXPostCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
